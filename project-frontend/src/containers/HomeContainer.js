2052067fee7fe1aa00a8dc2eb98628b3778599ee
github username <github email address>
2019-05-06 17:10:38 -0500

    olivia eod monday

--- project-frontend/src/containers/HomeContainer.js
+++ project-frontend/src/containers/HomeContainer.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import ProductCard from "../components/ProductCard";
 
 export default class HomeContainer extends React.Component{
 
     render(){
         return(
-            <div>
-
+            <div className="main-container">
+                <h1 className="main-text">THE SACRED SAGE</h1>
+                <h2 className="cursive-text">What's your spirit yearning for?</h2>
+                <button className="button">Begin Quiz</button>
             </div>
         )
     }
 }
+
